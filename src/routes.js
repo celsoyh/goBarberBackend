8c740dc1e40a4dd836ff0568b8eced14ee6cecd9
Celso Hashimoto <celsoyh@gmail.com>
2019-10-08 00:19:59 -0300

    Add User model / UserController to store user data

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,7 @@
 import { Router } from 'express';
-
+import UserController from './app/controllers/UserController';
 const routes = Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Server Structure working successfully!' });
-});
+routes.post('/user', UserController.store);
 
 export default routes;
